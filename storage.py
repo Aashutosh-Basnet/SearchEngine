51408fabced622726cb3eaad581cc708b98f959e
Aashutosh Basnet <aashutoshbasnet3344@gmail.com>
2024-11-28 15:18:29 +0530

    final commit

--- storage.py
+++ storage.py
@@ -1,15 +1,15 @@
 import sqlite3
 import pandas as pd
 
-class DB_storage():
+class DBStorage():
     def __init__(self):
-        self.con = sqlite3.connect("links.db")
+        self.con = sqlite3.connect('links.db')
         self.setup_tables()
 
     def setup_tables(self):
         cur = self.con.cursor()
         results_table = r"""
-            CREATE TABLE IF NOT EXISTS results(
+            CREATE TABLE IF NOT EXISTS results (
                 id INTEGER PRIMARY KEY,
                 query TEXT,
                 rank INTEGER,
@@ -18,23 +18,29 @@ class DB_storage():
                 snippet TEXT,
                 html TEXT,
                 created DATETIME,
-                relevance INTEGER.
-                UNIQUE(query, link) 
+                relevance INTEGER,
+                UNIQUE(query, link)
             );
-        """
-
+            """
         cur.execute(results_table)
         self.con.commit()
         cur.close()
 
-    def query_result(self, query):
-        df = pd.read_sql(f"select * from results where query = '{query}' order by rank asc;", self.con)
+    def query_results(self, query):
+        df = pd.read_sql(f"select * from results where query='{query}' order by rank asc", self.con)
         return df
 
     def insert_row(self, values):
         cur = self.con.cursor()
         try:
-            cur.execute('INSERT INTO results (query, rank, link, title, snippet, html, created) values (?,?,?,?,?,?,?)', values)
+            cur.execute('INSERT INTO results (query, rank, link, title, snippet, html, created) VALUES(?, ?, ?, ?, ?, ?, ?)', values)
+            self.con.commit()
         except sqlite3.IntegrityError:
             pass
+        cur.close()
+
+    def update_relevance(self, query, link, relevance):
+        cur = self.con.cursor()
+        cur.execute('UPDATE results SET relevance=? WHERE query=? AND link=?', [relevance, query, link])
+        self.con.commit()
         cur.close()
